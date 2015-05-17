bc533a0b7c416d38df0af97b2f955ccb32bd9087
Gilman Tolle <gilman.tolle@gmail.com>
2015-05-17 10:06:22 -0700

    Added the code to set up the window properly before creating the first view controller. For later.

--- Donnie/AppDelegate.swift
+++ Donnie/AppDelegate.swift
@@ -17,7 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         var vc = ViewController()
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
         window?.rootViewController = vc
+        window?.makeKeyAndVisible()
         return true
     }
 
